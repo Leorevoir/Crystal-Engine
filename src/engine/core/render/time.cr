3921d5e8039977ee865d6e431b4e9acbd57b53bb
= <=>
2025-01-17 21:20:52 +0100

    fix: time is a float64 for more precise output

--- src/engine/core/render/time.cr
+++ src/engine/core/render/time.cr
@@ -1,9 +1,10 @@
 module RenderLoop
 
     ####################################################################
-    # DeltaTime #TODO
+    # DeltaTime
+    # Float64 to be have more precise time inputs
     struct DeltaTime
-        @delta : Float64 = 0.0f64
+        @delta : Float64 = 0.0_f64
 
         def initialize(@frame_time : Float64, @passed_time : Float64, @start_time : Float64)
             @delta = Time.monotonic.total_seconds
